9ed83dea202db43edb8cf714124ccd0b26c0ac5b
GaganTut <gstut@live.com>
2017-03-14 18:52:16 -1000

    Finally Passed this one after lots of debugging

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,13 @@
 'use strict';
-module.exports = function() {
-  
-};
+module.exports = function(){
+  function createNumber() {
+    return Math.floor((Math.random() * 1000000) + 1);
+  }
+  var _number = createNumber();
+
+  function generateNumber() {
+    return _number;
+  }
+
+  return generateNumber;
+  };
